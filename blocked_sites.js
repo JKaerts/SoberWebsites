801dc1f2bbdf14642960f5160c68c97aa9cb33cb
JKaerts <jonaskaerts@gmail.com>
2021-11-18 10:43:46 +0100

    Add userscript to block websites

---
+++ blocked_sites.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Website blocklist
+// @namespace    https://github.com/JKaerts/SoberWebsites
+// @version      1.1
+// @description  My personal blocklist
+// @author       Jonas Kaerts
+// @match        https://www.reddit.com/*
+// @match        https://old.reddit.com/*
+// @match        https://*.ycombinator.com/*
+// @match        https://*.algolia.com/*
+// @match        https://lobste.rs/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Entirely clear the body
+    var body = document.body;
+    body.innerHTML = "";
+})();
